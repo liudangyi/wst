ffc22c4df8153a9c2bbc895bcc9a49cb92985c42
刘当一 <LeedyPKU@gmail.com>
2014-10-31 21:44:07 +0800

    hw4

---
+++ app/hw4-parties.php
@@ -0,0 +1,85 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
+$db = mysql_connect("localhost", "usr_2014_5", "internetics");
+if (!$db) {
+    print "Error - Could not connect to MySQL";
+    exit;
+}
+$er = mysql_select_db("db_2014_5");
+if (!$er) {
+    print "Error - Could not select the guest database";
+    exit;
+}
+if ($_REQUEST["time"]) {
+  $time = mysql_escape_string($_REQUEST['time']);
+  $place = mysql_escape_string($_REQUEST['place']);
+  $hostname = mysql_escape_string($_REQUEST['hostname']);
+  mysql_query("INSERT INTO party VALUES (NULL, '$time', '$place', '$hostname')");
+}
+if ($id = intval($_REQUEST["delete"])) {
+  mysql_query("delete from `guest-party` where Party_Num = $id");
+  mysql_query("delete from `party` where Party_Num = $id");
+}
+?><!DOCTYPE html>
+<title>Manage Parties</title>
+<link href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="styles/446ce9bb.main.css">
+<!-- build:css(.tmp) styles/main.css -->
+<link rel="stylesheet" href="styles/main.css" />
+<!-- endbuild -->
+<style>
+  #form {
+    width: 500px;
+    margin: 20px auto;
+  }
+</style>
+<div class="container">
+  <h1>Manage Parties</h1>
+  <form id="form" class="form-horizontal" method="post">
+    <div class="form-group">
+      <label for="time" class="col-sm-3 control-label">Time</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" id="time" name="time">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="place" class="col-sm-3 control-label">Place</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" id="place" name="place">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="hostname" class="col-sm-3 control-label">Host name</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" id="hostname" name="hostname">
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-3 col-sm-9">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+  <table class="table">
+    <thead>
+      <tr>
+        <td>ID</td>
+        <th>Time</th>
+        <th>Place</th>
+        <th>Host name</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php $result = mysql_query("select * from party"); while ($rec = mysql_fetch_array($result)) { ?>
+      <tr>
+        <td><?php echo $rec[0] ?></td>
+        <td><?php echo $rec[1] ?></td>
+        <td><?php echo $rec[2] ?></td>
+        <td><?php echo $rec[3] ?></td>
+        <td><a href="?delete=<?php echo $rec[0] ?>">delete</a></td>
+      </tr>
+    <?php } ?>
+    </tbody>
+  </table>
+</div>
--- app/hw4.php
+++ app/hw4.php
@@ -1,30 +1,139 @@
-<h1>WIP!!</h1>
 <?php
-error_reporting(E_ALL ^ E_DEPRECATED);
-
-$query = "SELECT Guest_Name, Gender from guest";
-
+error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
 $db = mysql_connect("localhost", "usr_2014_5", "internetics");
-
 if (!$db) {
     print "Error - Could not connect to MySQL";
     exit;
 }
-
 $er = mysql_select_db("db_2014_5");
-
 if (!$er) {
     print "Error - Could not select the guest database";
     exit;
 }
-$result = mysql_query($query);
-$num_rows = mysql_num_rows($result);
-print $result;
-for($row_num=0; $row_num < $num_rows; $row_num ++){
-    $row=mysql_fetch_array($result);
-    print "<p> Result row number " . ($row_num +1)." Geust_Name:";
-    print htmlspecialchars($row["Guest_Name"]);
-    print "  Gender:";
-    print htmlspecialchars($row["Gender"]);
-    print "</p>";
+if ($_REQUEST["name"]) {
+  $name =mysql_escape_string($_REQUEST['name']);
+  $age = mysql_escape_string($_REQUEST['age']);
+  $gender = mysql_escape_string($_REQUEST['gender']);
+  $email = mysql_escape_string($_REQUEST['email']);
+  mysql_query("INSERT INTO guest VALUES (NULL, '$name', '$age', '$gender', '$email')");
+  if ($_REQUEST["parties"]) {
+    $id = mysql_fetch_array(mysql_query("SELECT Guest_ID FROM guest ORDER BY Guest_ID DESC LIMIT 1"))[0];
+    foreach ($_REQUEST["parties"] as $key => $value) {
+      mysql_query("INSERT INTO `guest-party` VALUES ('$id', '$key')");
+    }
+  }
 }
+if ($id = intval($_REQUEST["delete"])) {
+  mysql_query("delete from `guest-party` where Guest_ID = $id");
+  mysql_query("delete from `guest` where Guest_ID = $id");
+}
+?><!DOCTYPE html>
+<title>Guest Book</title>
+<link href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="styles/446ce9bb.main.css">
+<!-- build:css(.tmp) styles/main.css -->
+<link rel="stylesheet" href="styles/main.css" />
+<!-- endbuild -->
+<style>
+  #form {
+    width: 500px;
+    margin: 20px auto;
+  }
+</style>
+<div class="container">
+  <h1>Guest Book</h1>
+  <form id="form" class="form-horizontal" method="post">
+    <div class="form-group">
+      <label for="name" class="col-sm-2 control-label">Name</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="name" name="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="age" class="col-sm-2 control-label">Age</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="age" name="age">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="age" class="col-sm-2 control-label">Gender</label>
+      <div class="col-sm-10">
+        <select class="form-control" name="gender">
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="email" class="col-sm-2 control-label">Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="email" name="email">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-2 control-label">Parties</label>
+      <div class="col-sm-10">
+        <div class="checkbox">
+        <?php $result = mysql_query("select * from party"); while ($rec = mysql_fetch_array($result)) { ?>
+          <label><input type="checkbox" name="parties[<?php echo $rec[0] ?>]"><?php echo $rec[1], ", ", $rec[2], ", ", $rec[3] ?></label>
+        <?php } ?>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Gender</th>
+        <th>Email</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php $result = mysql_query("select * from guest"); while ($rec = mysql_fetch_array($result)) { ?>
+      <tr>
+        <td><?php echo $rec[1] ?></td>
+        <td><?php echo $rec[2] ?></td>
+        <td><?php echo $rec[3] ?></td>
+        <td><?php echo $rec[4] ?></td>
+        <td>
+          <a href="?id=<?php echo $rec[0] ?>">activity(<?php echo mysql_fetch_array(mysql_query("select count(*) from `guest-party` where Guest_ID = $rec[0]"))[0]; ?>)</a> |
+          <a href="?delete=<?php echo $rec[0] ?>">delete</a>
+        </td>
+      </tr>
+    <?php } ?>
+    </tbody>
+  </table>
+  <?php if ($id = intval($_REQUEST["id"])) { ?>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Time</th>
+        <th>Place</th>
+        <th>Host name</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php $result = mysql_query(stripslashes("select * from `guest-party` gp, party p, guest g where gp.Guest_ID = g.Guest_ID and gp.Party_Num = p.Party_Num and g.Guest_ID = $id"));
+          while ($rec = mysql_fetch_array($result)) { ?>
+      <tr>
+        <td><?php echo $rec[7] ?></td>
+        <td><?php echo $rec[3] ?></td>
+        <td><?php echo $rec[4] ?></td>
+        <td><?php echo $rec[5] ?></td>
+      </tr>
+    <?php } ?>
+    </tbody>
+  </table>
+  <?php } ?>
+</div>
+
